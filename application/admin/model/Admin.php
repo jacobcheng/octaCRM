6e5da7bb2577fc29ced62d9106b9a2f8c8dd31ef
Jacob Zheng <im.jacobz@outlook.com>
2019-07-16 11:25:56 +0800

    管理员模型改为软删除

--- application/admin/model/Admin.php
+++ application/admin/model/Admin.php
@@ -4,15 +4,18 @@ namespace app\admin\model;
 
 use think\Model;
 use think\Session;
+use traits\model\SoftDelete;
 
 class Admin extends Model
 {
+    use SoftDelete;
 
     // 开启自动写入时间戳字段
     protected $autoWriteTimestamp = 'int';
     // 定义时间戳字段名
     protected $createTime = 'createtime';
     protected $updateTime = 'updatetime';
+    protected $deleteTime = 'deletetime';
 
     /**
      * 重置用户密码
